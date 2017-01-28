867ced59ace6b7bd53813a03cff736160f882712
Helio Oliveira <hheelliioo@live.com>
2017-01-28 20:51:29 -0200

    Add searches controller

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,5 @@
+class SearchesController < ApplicationController
+  def index
+    render json: {id: 1}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   get '/about', to: 'visitors#about', as: :about
+  get '/searches', to: 'searches#index', as: :searches
 end
